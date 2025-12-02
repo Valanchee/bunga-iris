4bc9c9a072165279b08f603f9074cc013dec2650
Valanchee <jerald1hincell23@gmail.com>
2025-12-02 12:16:51 +0700

    Update model.py

--- data/model.py
+++ data/model.py
@@ -1,36 +1,27 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import joblib
 
-# random seed
 seed = 42
 
-# Read original dataset
 iris_df = pd.read_csv("data/iris.csv")
 iris_df.sample(frac=1, random_state=seed)
 
-# selecting features and target data
 X = iris_df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
-y = iris_df[['Species']]
+y = iris_df['Species']
 
-# split data into train and test sets
-# 70% training and 30% test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=seed, stratify=y)
 
-knn = KNeightborsClassifier(n_neightbors=5)
+clf = KNeighborsClassifier(n_neighbors=5)
 
-# train the classifier on the training data
-knn.fit(X_train, y_train)
+clf.fit(X_train, y_train)
 
-# predict on the test set
 y_pred = clf.predict(X_test)
 
-# calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")  # Accuracy: 0.91
+print(f"Accuracy: {accuracy}")
 
-# save the model to disk
-joblib.dump(knn, "knn_model.sav")
+joblib.dump(clf, "knn_model.sav")
